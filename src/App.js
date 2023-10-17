81ab34dcf10ade8582e97cb9fdace6856f174eec
Chelsea Quindipan <quindipc@gmail.com>
2023-10-17 12:43:18 -0400

    Add firebase to app & edit homepage bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home/Home";
+import HomePage from "./Pages/HomePage/HomePage";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTDShSWIh1FCcfrOTjP6JCPkg0UlEpNyI",
+  authDomain: "coffee-chat-a47df.firebaseapp.com",
+  projectId: "coffee-chat-a47df",
+  storageBucket: "coffee-chat-a47df.appspot.com",
+  messagingSenderId: "433208987432",
+  appId: "1:433208987432:web:130287fea3c4e8d8a18a1d",
+  measurementId: "G-YJ6XZ3QFMH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   );
 }
