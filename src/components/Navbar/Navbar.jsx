7a91fc62a7ace839b8d49d43fc729a771a95a967
Jennifer Chou <63386589+codecaviette@users.noreply.github.com>
2023-10-25 14:13:16 -0700

    Add Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li><a href="#">Connections</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Calendar</a></li>
+        <li><a href="#">Profile</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import "./HomePage.scss";
+import Navbar from "../../components/Navbar/Navbar";
 
 const HomePage = () => {
   return (
     <div className="home">
       <h1 className="home__title"> Welcome !</h1>
+      <Navbar />
     </div>
   );
 };
