ad77ca2dafea8773df23e6e6d79c04dd9c9fef34
Chelsea Quindipan <quindipc@gmail.com>
2023-10-26 13:06:14 -0400

    add dummy data and chat route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import ConnectionsPage from "./pages/ConnectionsPage/ConnectionsPage";
+import Chat from "./components/Chat/Chat";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,12 +26,40 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// Dummy Data for Chat 
+const connectionsData = [
+  {
+    id: 1,
+    name: "John Doe",
+    profilePicture: "john.jpg",
+  },
+  {
+    id: 2,
+    name: "Alice Smith",
+    profilePicture: "alice.jpg",
+  },
+  {
+    id: 3,
+    name: "Julie Tall",
+    profilePicture: "julie.jpg",
+  },
+  {
+    id: 5,
+    name: "George Small",
+    profilePicture: "george.jpg",
+  },
+];
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/connections" element={<ConnectionsPage people={connectionsData} />} />
+      <Route path="/chat/:id" element={<Chat people={connectionsData} db={db}/>} />
     </Routes>
   );
 }
