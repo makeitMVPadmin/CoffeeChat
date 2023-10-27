ce200981e9af907fd5e8b4ff47ab9934254ffbb4
Yaira V <39809411+Yvv1214@users.noreply.github.com>
2023-10-27 02:21:41 +0000

    landingpage header/schedule done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,64 @@
 import "./HomePage.scss";
 import Navbar from "../../components/Navbar/Navbar";
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  StyleSheet,
+  View,
+  ImageBackground,
+  Animated,
+  useWindowDimensions,
+} from 'react-native';
+
+
+
+
 
 const HomePage = () => {
+
+  
+
+
+
+
   return (
-    <div className="home">
-      <h1 className="home__title"> Welcome !</h1>
+    <View className="home">
+      <div className="homeHeader">
+        <h1 className="WelcomeTitle">Welcome, <br></br> name!</h1>
+        <div className="accountBtn">account</div>
+      </div>
+
+      <div className="carousel">
+        
+
+      </div>
+
+      <h3 style={{textAlign: 'center'}} >Schedule</h3>
+
+      <div className="scheduleDiv">
+        <div className="scheduleList">
+          <div className="scheduledName">Richard Yin</div>
+          <div>October 17 4:30pm (virtual)</div>
+        </div>
+
+        <div className="scheduleList">
+          <div className="scheduledName">Richard Yin</div>
+          <div>October 17 4:30pm (virtual)</div>
+        </div>
+
+      </div>
+
+      <div>
+        <div> new connections</div>
+        <div> interactions</div>
+      </div>
+
+      
+
+
       <Navbar />
-    </div>
+    </View>
   );
 };
 
