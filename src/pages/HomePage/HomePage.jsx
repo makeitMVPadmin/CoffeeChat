3b85f978f21d5b64dc480b84636778c02a6a5ff5
Yaira V <39809411+Yvv1214@users.noreply.github.com>
2023-10-28 01:21:21 +0000

    added lower section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
 import "./HomePage.scss";
 import Navbar from "../../components/Navbar/Navbar";
+import Slider from 'slider-moon';
+import 'slider-moon/dist/style.css'
 import {
   SafeAreaView,
   ScrollView,
@@ -17,24 +19,52 @@ import {
 
 const HomePage = () => {
 
-  
 
-
+  const items = [
+    {
+      id: '12',
+      img: 'https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    // {
+    //   id: '11',
+    //   img: 'https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=600'
+    // },
+  ]
 
 
   return (
     <View className="home">
+      <ScrollView>
       <div className="homeHeader">
         <h1 className="WelcomeTitle">Welcome, <br></br> name!</h1>
-        <div className="accountBtn">account</div>
+        <div className="accountBtn"></div>
       </div>
+
+      {/* <Slider
+        slideClass={'my-slider'}
+        infinite={true}
+        
+        animation={'scale'}
+        callback={() => {
+          console.log('here');
+        }}
+      >
+        <div className='slider my-slider'>
+          <ul className='slider-wrapper'>
+          {items.map((item) => (
+          <li key={item.id}>
+            <img src={item.img} />
+          </li>
+        ))}
+          </ul>
+        </div>
+      </Slider> */}
 
       <div className="carousel">
-        
-
+        carousel
       </div>
 
-      <h3 style={{textAlign: 'center'}} >Schedule</h3>
+      <h3 style={{ textAlign: 'center' }} >Schedule</h3>
 
       <div className="scheduleDiv">
         <div className="scheduleList">
@@ -49,14 +79,14 @@ const HomePage = () => {
 
       </div>
 
-      <div>
-        <div> new connections</div>
-        <div> interactions</div>
+      <div className="lowerDiv">
+        <div className="connections"> new connections</div>
+        <div className="connections"> interactions</div>
       </div>
 
-      
 
 
+      </ScrollView>
       <Navbar />
     </View>
   );
