972d1ca3a7719a5cdd6f6b59383985ecdb963763
Chelsea Quindipan <quindipc@gmail.com>
2023-11-03 08:20:56 -0400

    Add skeleton & styling

--- src/components/Landing/Header/Header.jsx
+++ src/components/Landing/Header/Header.jsx
@@ -1,14 +1,22 @@
+import "./Header.scss";
+import coffeechatLogo from "../../../assets/logo/logo.png";
+
 const Header = () => {
   return (
     <>
       <header className="header">
         <nav className="header__nav">
-          <img className="header__logo" src="#" alt="logo" />
           <ul className="header__navlist">
-            <li className="header__navlist--item">About</li>
-            <li className="header__navlist--item">FAQs</li>
-            <li className="header__navlist--item">Privacy</li>
-            <li className="header__navlist--item">Contact</li>
+            <div className="header__container--logo">
+              <img className="header__logo" src={coffeechatLogo} alt="logo" />
+            </div>
+
+            <div className="header__container--list">
+              <li className="header__navlist--item">About</li>
+              <li className="header__navlist--item">FAQs</li>
+              <li className="header__navlist--item">Privacy</li>
+              <li className="header__navlist--item">Contact</li>
+            </div>
           </ul>
         </nav>
       </header>
