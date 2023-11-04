60ecedc9946883578ddfdef895874b2295eea8bc
Chelsea Quindipan <quindipc@gmail.com>
2023-11-04 13:09:10 -0400

    add hero jsx and scss

---
+++ src/components/Landing/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import "./Hero.scss";
+
+const Hero = () => {
+
+
+  return (
+      <>
+          <section className="hero">
+              
+
+              <button className="hero__button">Sign Up for Email Notifications</button>
+          </section>  
+      </>
+  );
+};
+
+export default Hero;
