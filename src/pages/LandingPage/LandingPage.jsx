86e40b64ee5987fcc156248433c220e3b41db028
Chelsea Quindipan <quindipc@gmail.com>
2023-11-06 18:32:25 -0500

    add to landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Landing/Header/Header";
 import Hero from "../../components/Landing/Hero/Hero";
+import About from "../../components/Landing/About/About";
 
 const LandingPage = () => {
   return (
       <>
       <Header />
       <Hero />
+      <About />
       </>
   );
 };
