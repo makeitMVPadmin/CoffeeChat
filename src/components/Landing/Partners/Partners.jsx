90cc9f91430bcbb175aeaf0d1d441dce3d799fae
Chelsea Quindipan <quindipc@gmail.com>
2023-11-08 10:28:03 -0500

    add images & skeleton

--- src/components/Landing/Partners/Partners.jsx
+++ src/components/Landing/Partners/Partners.jsx
@@ -1,9 +1,38 @@
 import "./Partners.scss";
+import phoneMockup from "../../../assets/images/Partners Mockup.png";
+import lastingImage from "../../../assets/images/lasting-connections.png";
+import professionalImage from "../../../assets/images/professional.png";
 
 const Partners = () => {
   return (
     <>
-      
+      <section className="partners">
+        <h2 className="partners__title">Partners we work with</h2>
+
+        <img
+          src={phoneMockup}
+          alt="Phone holding mockup"
+          className="partners__mockup"
+        />
+
+        <h3 className="partners__description">
+          <span className="partners__highlight">CoffeeChat</span> leverages AI
+          into finding you a perfect professional match, no more connections
+          that leads nowhere
+        </h3>
+
+        <img
+          src={lastingImage}
+          alt="two women connecting"
+          className="partners__image"
+        />
+        <img
+          src={professionalImage}
+          alt="two people collaborating"
+          className="partners__image"
+              />
+              
+      </section>
     </>
   );
 };
