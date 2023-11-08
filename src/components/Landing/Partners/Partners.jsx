c1bc39a8b782454a8d34d9739ea6a81dd06c2ba6
Chelsea Quindipan <quindipc@gmail.com>
2023-11-08 16:48:26 -0500

    add makeitmvp logo

--- src/components/Landing/Partners/Partners.jsx
+++ src/components/Landing/Partners/Partners.jsx
@@ -2,36 +2,45 @@ import "./Partners.scss";
 import phoneMockup from "../../../assets/images/Partners Mockup.png";
 import lastingImage from "../../../assets/images/lasting-connections.png";
 import professionalImage from "../../../assets/images/professional.png";
+import makeitmvpLogo from "../../../assets/images/makeitmvp.png";
 
 const Partners = () => {
   return (
     <>
       <section className="partners">
-        <h2 className="partners__title">Partners we work with</h2>
+        <div className="partners__container--header">
+          <h2 className="partners__title">Partners we work with</h2>
+          <img
+            src={makeitmvpLogo}
+            alt="makeitMVP logo"
+            className="partners__partner"
+          />
+        </div>
 
-        <img
-          src={phoneMockup}
-          alt="Phone holding mockup"
-          className="partners__mockup"
-        />
+        <div className="partners__container--info">
+          <img
+            src={phoneMockup}
+            alt="Phone holding mockup"
+            className="partners__mockup"
+          />
 
-        <h3 className="partners__description">
-          <span className="partners__highlight">CoffeeChat</span> leverages AI
-          into finding you a perfect professional match, no more connections
-          that leads nowhere
-        </h3>
+          <h3 className="partners__description">
+            <span className="partners__highlight">CoffeeChat</span> leverages AI
+            into finding you a perfect professional match, no more connections
+            that leads nowhere
+          </h3>
 
-        <img
-          src={lastingImage}
-          alt="two women connecting"
-          className="partners__image"
-        />
-        <img
-          src={professionalImage}
-          alt="two people collaborating"
-          className="partners__image"
-              />
-              
+          <img
+            src={lastingImage}
+            alt="two women connecting"
+            className="partners__image"
+          />
+          <img
+            src={professionalImage}
+            alt="two people collaborating"
+            className="partners__image"
+          />
+        </div>
       </section>
     </>
   );
