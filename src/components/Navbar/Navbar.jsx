eacfbc9991ad448b319dcc382d63d6febba05431
unknown <94neongreen@gmail.com>
2023-11-08 01:50:43 -0500

    style navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,35 @@
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
+import home from '../../assets/icons/navbar/home.svg'
+import chat from '../../assets/icons/navbar/chat.svg'
+import calendar from '../../assets/icons/navbar/calendar.svg'
+import user from '../../assets/icons/navbar/user.svg'
+
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/connections">Connections</Link>
-        </li>
-        <li>
-          <Link to="/calendar">Calendar</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-      </ul>
+
+      <div>
+        <div><Link to="/"><img className="navIcons" src={home} /></Link></div>
+        <Link to="/" className="navLink">Home</Link>
+      </div>
+
+      <div>
+        <div><Link to="/"><img className="navIcons" src={chat} /></Link></div>
+        <Link to="/connections" className="navLink">Connections</Link>
+      </div>
+
+      <div>
+        <div><Link to="/"><img className="navIcons" src={calendar} /></Link></div>
+        <Link to="/calendar" className="navLink">Calendar</Link>
+      </div>
+
+      <div>
+        <div><Link to="/"><img className="navIcons" src={user} /></Link></div>
+        <Link to="/profile" className="navLink">Profile</Link>
+      </div>
+
     </div>
   );
 };
