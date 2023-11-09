1c5b02780bdea5fad8fd734b009db971ea92b53b
Chelsea Quindipan <quindipc@gmail.com>
2023-11-09 13:53:47 -0500

    add features styling

--- src/components/Landing/Features/Features.jsx
+++ src/components/Landing/Features/Features.jsx
@@ -2,27 +2,43 @@ import "./Features.scss";
 
 const Features = () => {
   return (
-      <section className="features">
-          <div className="features__box">
-      <h2 className="features__title">Features</h2>
-      <hr className="features__line" />
-      <h3 className="features__subtitle">Smart Matching Algorithm</h3>
-      <p className="features__description">
-        Ensuring you meet the right people at the right time
-      </p>
-      <h3 className="features__subtitle">Integrated Calendar</h3>
-      <p className="features__description">
-        Never double book or miss an opportunity
-      </p>
-      <h3 className="features__subtitle">In-app Chat</h3>
-      <p className="features__description">
-        Seamlessly coordinate and break the ice before the meeting
-      </p>
-      <h3 className="features__subtitle">Privacy Controls</h3>
-      <p className="features__description">
-        Share what you want, when you want
-      </p>
+    <section className="features">
+      <div className="features__box">
+        <div className="features__container--title">
+          <h2 className="features__title">Features</h2>
+        </div>
+        <div className="features__row">
+          <div className="features__column">
+            <div className="features__container">
+              <h3 className="features__subtitle">Smart Matching Algorithm</h3>
+              <p className="features__description">
+                Ensuring you meet the right people at the right time
+              </p>
+            </div>
+            <div className="features__container">
+              <h3 className="features__subtitle">In-app Chat</h3>
+              <p className="features__description">
+                Seamlessly coordinate and break the ice before the meeting
+              </p>
+            </div>
           </div>
+
+          <div className="features__column">
+            <div className="features__container">
+              <h3 className="features__subtitle">Integrated Calendar</h3>
+              <p className="features__description">
+                Never double book or miss an opportunity
+              </p>
+            </div>
+            <div className="features__container">
+              <h3 className="features__subtitle">Privacy Controls</h3>
+              <p className="features__description">
+                Share what you want, when you want
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
