9a7822a6101e4c767b9927573ab5958604a80996
Chelsea Quindipan <quindipc@gmail.com>
2023-11-09 15:14:06 -0500

    add skeleton & styling

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,19 @@
 import Header from "../../components/Landing/Header/Header";
 import Hero from "../../components/Landing/Hero/Hero";
+// import About from "../../components/Landing/About/About";
+// import Partners from "../../components/Landing/Partners/Partners";
+// import Features from "../../components/Landing/Features/Features";
+import Testimonials from "../../components/Landing/Testimonials/Testimonials";
 
 const LandingPage = () => {
   return (
       <>
       <Header />
       <Hero />
+      {/* <About /> */}
+      {/* <Partners /> */}
+      {/* <Features /> */}
+      <Testimonials />
       </>
   );
 };
