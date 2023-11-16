17e7c815beb9d2a9fa73bb7d1ec5f88553cb7f1c
Kyla Ester <esterkyla@outlook.com>
2023-11-15 20:22:04 -0500

    Working on Onboarding Login and Sign up

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -57,67 +57,69 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="centerForm">
-      <img className="logoMark" src={FinalLogo}></img>
+    <section className="login">
+      <div className="centerForm">
+        <img className="logoMark" src={FinalLogo}></img>
 
-      <h2 className="loginH2">Welcome Back!</h2>
+        <h2 className="loginH2">Welcome Back!</h2>
 
-      <form className="loginForm" onSubmit={signIn}>
-        <input
-          className="inputLogin"
-          type="email"
-          placeholder="Enter your email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <form className="loginForm" onSubmit={signIn}>
+          <input
+            className="inputLogin"
+            type="email"
+            placeholder="Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-        <input
-          className="inputLogin"
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <input
+            className="inputLogin"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <div className="checkBoxLoginDiv">
-          <input className="checkBoxLogin" type="checkbox" />
-          <p>Remember me?</p>
+          <div className="checkBoxLoginDiv">
+            <input className="checkBoxLogin" type="checkbox" />
+            <p>Remember me?</p>
+          </div>
+        </form>
+
+        <button className="loginBtn" type="submit" onClick={signIn}>
+          Login
+        </button>
+
+        <a style={{ zIndex: "1" }} href="needToFill">
+          Forgot Password?
+        </a>
+
+        <div className="continueWith">
+          <p>Or continue with</p>
         </div>
-      </form>
 
-      <button className="loginBtn" type="submit" onClick={signIn}>
-        Login
-      </button>
+        <div className="altBtns">
+          <div className="border">
+            <button className="loginBtn" type="button" onClick={GoogleSignIn}>
+              <img src={google} className="linkSvg" />
+              <img src={googletext} className="linkText" />
+            </button>
+          </div>
+          <div className="border">
+            <button className="loginBtn" type="button">
+              <img src={linkedin} className="linkSvg" />
+              <img src={linkedintext} className="linkText" />
+            </button>
+          </div>
+        </div>
 
-      <a style={{ zIndex: "1" }} href="needToFill">
-        Forgot Password?
-      </a>
+        <p className="continueWith">Don't have an account?</p>
+        <a style={{ zIndex: "1" }} href="/signup">
+          Sign Up
+        </a>
 
-      <div className="continueWith">
-        <p>Or continue with</p>
+        <div className="backgroundLogin"></div>
+        <div className="backgroundLogin2"></div>
       </div>
-
-      <div className="altBtns">
-        <div className="border">
-          <button className="loginBtn" type="button" onClick={GoogleSignIn}>
-            <img src={google} className="linkSvg" />
-            <img src={googletext} className="linkText" />
-          </button>
-        </div>
-        <div className="border">
-          <button className="loginBtn" type="button">
-            <img src={linkedin} className="linkSvg" />
-            <img src={linkedintext} className="linkText" />
-          </button>
-        </div>
-      </div>
-
-      <p className="continueWith">Don't have an account?</p>
-      <a style={{ zIndex: "1" }} href="/signup">
-        Sign Up
-      </a>
-
-      <div className="backgroundLogin"></div>
-      <div className="backgroundLogin2"></div>
-    </div>
+    </section>
   );
 };
 
