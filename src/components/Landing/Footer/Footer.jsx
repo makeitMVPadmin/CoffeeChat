c8e32f208c5fd2061b0466ef60760669b7d5aa17
Chelsea Quindipan <quindipc@gmail.com>
2023-11-15 21:25:30 -0500

    add styles

--- src/components/Landing/Footer/Footer.jsx
+++ src/components/Landing/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Footer.scss";
 import coffeechatLogo from "../../../assets/logo/logo.png";
@@ -7,85 +6,28 @@ const Footer = () => {
   const navLinks = [
     { to: "#About", text: "About" },
     { to: "#FAQs", text: "FAQs" },
-    { to: "#Privacy", text: "Privacy" },
-    { to: "Contact", text: "Contact" },
+    { to: "/landing-page/Privacy", text: "Privacy" },
+    { to: "/landing-page/Contact", text: "Contact" },
   ];
 
-  const desktopNavLinks = [
-    { to: "#About", text: "About" },
-    { to: "#FAQs", text: "FAQs" },
-    { to: "#Privacy", text: "Privacy" },
-    { to: "Contact", text: "Contact" },
-  ];
+  const renderNavLinks = (links) =>
+    links.map((link, index) => (
+      <li key={index} className="footer__list-item">
+        <Link className="footer__list-item" to={link.to}>{link.text}</Link>
+      </li>
+    ));
 
   return (
     <footer className="footer">
       <div className="footer__container">
-        <div className="footer__column">
+        <div className="footer__container--logo">
           <img src={coffeechatLogo} alt="Logo" className="footer__logo" />
         </div>
-        <div className="footer__column">
-          <ul>
-            <li>
-              <Link to="/landing-page">Home</Link>
-            </li>
-            <li>
-              <Link to="/landing-page/about">Popular</Link>
-            </li>
-            <li>
-              <Link to="/landing-page/services">About</Link>
-            </li>
+        <nav className="footer__list">
+          <ul className="footer__items">
+            {renderNavLinks(navLinks)}
           </ul>
-        </div>
-        <div className="footer__column">
-          <ul>
-            <li>
-              <Link to="/landing-page/faq">FAQs</Link>
-            </li>
-            <li>
-              <Link to="/landing-page/privacy">Privacy</Link>
-            </li>
-            <li>
-              <Link to="/landing-page/socials">Socials</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="footer__column">
-          <ul>
-            <li>
-              <Link to="/landing-page/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="footer__column">
-          <ul>
-            <li>Follow us</li>
-            <div className="footer__icons">
-              <li>
-                <img src="" alt="twitter" />
-              </li>
-              <li>
-                <img src="" alt="facebook" />
-              </li>
-              <li>
-                <img src="" alt="google" />
-              </li>
-              <li>
-                <img src="" alt="instagram" />
-              </li>
-            </div>
-
-            <div className="footer__store">
-              <li>
-                <img src="" alt="app store" />
-              </li>
-              <li>
-                <img src="" alt="google play" />
-              </li>
-            </div>
-          </ul>
-        </div>
+        </nav>
       </div>
     </footer>
   );
