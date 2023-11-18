461faa078000d8077d78f98c162a941f870ac9f0
Kyla Ester <esterkyla@outlook.com>
2023-11-18 15:38:24 -0500

    Got Filtered People Working With the filters

--- src/pages/ConnectionsPage/ConnectionsPage.jsx
+++ src/pages/ConnectionsPage/ConnectionsPage.jsx
@@ -1,25 +1,255 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './ConnectionsPage.scss';
-import logo from '../../assets/logo/FinalLogo.png';
-import Navbar from '../../components/Navbar/Navbar';
+import React from "react";
+import "./ConnectionsPage.scss";
+import { Link, useNavigate } from "react-router-dom";
+import guy from "../../assets/images/YellowGuyAnimation.png";
+import girl from "../../assets/images/PinkGirlAnimation.png";
+import Navbar from "../../components/Navbar/Navbar";
+import { useState, useEffect } from "react";
+// ConnectionsPage.jsx
+import { db } from "../../App";
+import "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+
+// Your component code...
 
 const ConnectionsPage = ({ people }) => {
+  const [filters, setFilters] = useState({
+    expertise: [],
+    discipline: [],
+    industry: [],
+    experience: [],
+  });
+  const navigate = useNavigate();
+  const [filteredPeople, setFilteredPeople] = useState([]);
+  const handleFilterChange = (filterType, value) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [filterType]: value,
+    }));
+  };
+
+  const handleClearFilters = () => {
+    setFilters({
+      expertise: [],
+      discipline: [],
+      industry: [],
+      experience: [],
+    });
+  };
+  const handleNextPage = () => {
+    // Pass the filteredPeople to the next page using state or URL params
+    navigate("/next", { state: { filteredPeople } });
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const collectionRef = collection(db, "Users");
+
+        if (!collectionRef) {
+          console.error("Firestore collection reference is undefined.");
+          return;
+        }
+
+        // Apply filters
+        let filteredQuery = collectionRef;
+
+        if (filters.expertise.length > 0) {
+          filteredQuery = query(
+            filteredQuery,
+            where("Expertise", "in", filters.expertise)
+          );
+        }
+
+        if (filters.discipline.length > 0) {
+          filteredQuery = query(
+            filteredQuery,
+            where("Discipline", "in", filters.discipline)
+          );
+        }
+
+        if (filters.industry.length > 0) {
+          filteredQuery = query(
+            filteredQuery,
+            where("Industry", "in", filters.industry)
+          );
+        }
+
+        if (filters.experience.length > 0) {
+          filteredQuery = query(
+            filteredQuery,
+            where("Experience", "in", filters.experience)
+          );
+        }
+
+        console.log(filteredQuery); // Add this line for debugging
+
+        const snapshot = await getDocs(filteredQuery);
+        const data = snapshot.docs.map((doc) => doc.data());
+
+        setFilteredPeople(data);
+      } catch (error) {
+        console.error("Error fetching data from Firestore:", error);
+      }
+    };
+
+    fetchData(); // Call fetchData directly as a one-time operation
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filters]);
+
+  useEffect(() => {
+    console.log(filters);
+  }, [filters]);
+  console.log(filters);
+  console.log(filteredPeople);
   return (
-    <div className='connections__container'>
-      <h3 className='connections--blue'>
-        Let's get you
-        <span className='connections--red'> connected!</span>
-      </h3>
-      <div className='connections__image'>
-        <img className='connections__image-img' src={logo} alt='' />
-      </div>
+    <section className="connections">
+      <div className="connections__container">
+        <h3 className="connections--blue">
+          Let's get you
+          <span className="connections--red"> connected!</span>
+        </h3>
+        <div className="connections__image">
+          <img className="connections__image-img-1" src={girl} alt="" />
+          <img className="connections__image-img-2" src={guy} alt="" />
+        </div>
 
-      <div className='connections__box'>
-        <p>Finding your best three matches...</p>
-      </div>
+        <div className="connections__box">
+          <div className="connections__filters">
+            {" "}
+            <p className="connections__titles">Expertise</p>
+            <div className="connections__options">
+              <p
+                className="connections__option-1"
+                onClick={() => handleFilterChange("expertise", ["Design"])}
+              >
+                Design
+              </p>
+              <p
+                className="connections__option-1"
+                onClick={() => handleFilterChange("expertise", ["Product"])}
+              >
+                Product
+              </p>
+            </div>
+          </div>
+          <div className="connections__filters">
+            {" "}
+            <p className="connections__titles">Discipline</p>
+            <div className="connections__options">
+              <p
+                className="connections__option-2"
+                onClick={() => handleFilterChange("discipline", ["UIDesign"])}
+              >
+                UI Design
+              </p>
+              <p
+                className="connections__option-2"
+                onClick={() =>
+                  handleFilterChange("discipline", ["ProductDesign"])
+                }
+              >
+                Product Design
+              </p>
+              <p
+                className="connections__option-2"
+                onClick={() => handleFilterChange("discipline", ["Operations"])}
+              >
+                Operations
+              </p>
+              <p
+                className="connections__option-2"
+                onClick={() => handleFilterChange("discipline", ["Sales"])}
+              >
+                Sales
+              </p>
+              <p
+                className="connections__option-2"
+                onClick={() => handleFilterChange("discipline", ["Marketing"])}
+              >
+                Marketing
+              </p>
+              <p
+                className="connections__option-2"
+                onClick={() => handleFilterChange("discipline", ["Strategy"])}
+              >
+                Strategy
+              </p>
+              <p
+                className="connections__option-2"
+                onClick={() =>
+                  handleFilterChange("discipline", ["Engineering"])
+                }
+              >
+                Engineering
+              </p>
+            </div>
+          </div>
 
-      {/* <ul>
+          <div className="connections__filters">
+            {" "}
+            <p className="connections__titles">Industry</p>
+            <div className="connections__options">
+              <p
+                className="connections__option-3"
+                onClick={() => handleFilterChange("industry", ["Tech"])}
+              >
+                Tech
+              </p>
+              <p
+                className="connections__option-3"
+                onClick={() => handleFilterChange("industry", ["Healthcare"])}
+              >
+                Healthcare
+              </p>
+              <p
+                className="connections__option-3"
+                onClick={() => handleFilterChange("industry", ["Finance"])}
+              >
+                Finance
+              </p>
+            </div>
+          </div>
+
+          <div className="connections__filters">
+            {" "}
+            <p className="connections__titles">Experience</p>
+            <div className="connections__options">
+              <p
+                className="connections__option-4"
+                onClick={() => handleFilterChange("experience", ["0-2"])}
+              >
+                0-2
+              </p>
+              <p
+                className="connections__option-4"
+                onClick={() => handleFilterChange("experience", ["2-4"])}
+              >
+                2-4
+              </p>
+              <p
+                className="connections__option-4"
+                onClick={() => handleFilterChange("experience", ["5+"])}
+              >
+                5+
+              </p>
+            </div>
+          </div>
+          <div>
+            {" "}
+            <button onClick={handleNextPage}>Next</button>
+            <button onClick={handleClearFilters}>Clear Filters</button>
+          </div>
+        </div>
+
+        {/* <ul>
         {people.map((person) => (
           <li key={person.id}>
             <Link to={`/chat/${person.id}`}>
@@ -30,8 +260,9 @@ const ConnectionsPage = ({ people }) => {
         ))}
       </ul> */}
 
-<Navbar/>
-    </div>
+        <Navbar />
+      </div>
+    </section>
   );
 };
 
