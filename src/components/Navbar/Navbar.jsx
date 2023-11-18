83910cf937ad28e9eeebe93b0ab3d5502a6aa439
Kyla Ester <esterkyla@outlook.com>
2023-11-18 00:18:54 -0500

    Updating Styles for Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,56 +1,62 @@
 import "./Navbar.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import homeicon from "../../assets/icons/navbar/homeicon.svg";
 import chat from "../../assets/icons/navbar/chat.svg";
 import calendar from "../../assets/icons/navbar/calendar.svg";
 import user from "../../assets/icons/navbar/User.svg";
+import logo from "../../assets/logo/Final_logo.svg";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div>
+      <img className="logo" src={logo} />
+      <NavLink
+        to="/home"
+        className={(navData) =>
+          navData.isActive ? "navLinksActive" : "navLinks"
+        }
+      >
         <div>
-          <Link to="/">
+          <Link to="/home">
             <img className="navIcons" src={homeicon} alt="Home icon" />
           </Link>
         </div>
-        <Link to="/" className="navLink">
+        <Link to="/home" className="navLink">
           Home
         </Link>
-      </div>
+      </NavLink>
 
-      <div>
+      <NavLink
+        to="/connections"
+        className={(navData) =>
+          navData.isActive ? "navLinksActive" : "navLinks"
+        }
+      >
         <div>
-          <Link to="/">
+          <Link to="/connections">
             <img className="navIcons" src={chat} alt="Chat Icon" />
           </Link>
         </div>
         <Link to="/connections" className="navLink">
-          Connections
+          Connect
         </Link>
-      </div>
+      </NavLink>
 
-      <div>
+      <NavLink
+        to="/profile"
+        className={(navData) =>
+          navData.isActive ? "navLinksActive" : "navLinks"
+        }
+      >
         <div>
-          <Link to="/">
-            <img className="navIcons" src={calendar} alt="Calender Icon" />
-          </Link>
-        </div>
-        <Link to="/calendar" className="navLink">
-          Calendar
-        </Link>
-      </div>
-
-      <div>
-        <div>
-          <Link to="/">
+          <Link to="/profile">
             <img className="navIcons" src={user} alt="User Icon" />
           </Link>
         </div>
         <Link to="/profile" className="navLink">
           Profile
         </Link>
-      </div>
+      </NavLink>
     </div>
   );
 };
