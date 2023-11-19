37ccb228a18ed6f7c34783f0cada1b493337e902
Chelsea Quindipan <quindipc@gmail.com>
2023-11-19 11:05:13 -0500

    edit testimonials styling

--- src/components/Landing/Testimonials/Testimonials.jsx
+++ src/components/Landing/Testimonials/Testimonials.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./Testimonials.scss";
+import avatarImage from "../../../assets/images/avatar (1).png";
 
 const Testimonials = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -9,51 +10,45 @@ const Testimonials = () => {
       name: "John Doe",
       role: "CEO",
       comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      icon: "",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      icon: avatarImage,
     },
     {
       name: "Jane Smith",
       role: "Designer",
       comment:
-        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      icon: "",
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      icon: avatarImage,
     },
     {
       name: "Bob Johnson",
       role: "Developer",
       comment:
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-      icon: "",
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      icon: avatarImage,
     },
   ];
 
   const handleDotClick = (index) => {
     setCurrentIndex(index);
-    };
+  };
 
-    const currentTestimonial = testimonials[currentIndex];
-
+  const currentTestimonial = testimonials[currentIndex];
 
   return (
     <section className="testimonials">
       <h2 className="testimonials__title">What Our Users Say</h2>
-          <div className="testimonials__carousel">
-              {testimonials.map((currentTestimonial, index) => (
-                  <div
-                      key={index}
-                      className={
-                          index === currentIndex
-                                  ? "testimonials__item active "
-                                  : "testimonials__item preview"
-                      }
-                  >
-                   <img src={currentTestimonial.icon} alt="icon" />
-          <h3>{currentTestimonial.name}</h3>
-          <p>{currentTestimonial.role}</p>
+      <div className="testimonials__carousel">
+        <div className="testimonials__item active">
+          <div className="testimonials__container">
+            <img src={currentTestimonial.icon} alt="icon" />
+            <div className="testimonials__container--title">
+              <h3>{currentTestimonial.name}</h3>
+              <p>{currentTestimonial.role}</p>
+            </div>
+          </div>
           <p>{currentTestimonial.comment}</p>
-                  </div>
-              ))}
+        </div>
       </div>
       <div className="testimonials__buttons">
         {testimonials.map((_, index) => (
@@ -71,6 +66,5 @@ const Testimonials = () => {
     </section>
   );
 };
-
 
 export default Testimonials;
