1d30f2e84cbbe19d7aedd1f95ca42f6ff7c407c2
Yvv1214 <94neongreen@gmail.com>
2024-01-31 20:46:27 -0500

    app.js fixed

--- src/App.js
+++ src/App.js
@@ -74,17 +74,7 @@ function App() {
   const auth = getAuth();
 
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setUser(user);
-      setLoading(false);
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+  const [loading, setLoading] = useState(false);
   
 
   if (loading) {
