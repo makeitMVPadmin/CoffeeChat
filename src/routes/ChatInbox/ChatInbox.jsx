8f0e97612134eca7d51ea5ab18622895b6783880
Kati Ealey <kati.majorealey@gmail.com>
2024-02-15 12:50:42 -0500

    chat page has been split up into a chat page/route and inbox page/route

---
+++ src/routes/ChatInbox/ChatInbox.jsx
@@ -0,0 +1,14 @@
+import React from "react"; 
+import Sidebar from "../../components/ChatFeature/Sidebar/Sidebar"
+
+
+const ChatInbox = () => {
+    return(
+        <div className="home">
+            <div className="container">
+                <Sidebar />
+            </div>
+        </div>
+    )
+}
+export default ChatInbox
--- src/routes/ChatPage/ChatPage.jsx
+++ src/routes/ChatPage/ChatPage.jsx
@@ -2,14 +2,11 @@
 
 import React from "react"; 
 import Chat from "../../components/ChatFeature/Chat/Chat"
-import Sidebar from "../../components/ChatFeature/Sidebar/Sidebar"
-import "./ChatPage.scss"
 
 const ChatPage = () => {
     return(
         <div className="home">
             <div className="container">
-                <Sidebar />
                 <Chat />
             </div>
         </div>
