59a27ad15c2548a9247d9ea96faf9c5cc9610717
Anthony Shellman <atshellman@gmail.com>
2024-02-16 14:02:01 -0700

    Initial commit

---
+++ src/data/connectionsPageData.jsx
@@ -0,0 +1,21 @@
+// Dummy Data for ConnectionsPage
+import RajDevPicture from "../assets/images/RajDev.svg"
+
+const connectionsPageData = [
+  {
+    id: 1,
+    name: "Raj Dev",
+    profilePicture: RajDevPicture,
+    bio: "I am a Software dev with over 2 years of experience. I believe in mentoring juniors and helping them reach their professional goals. When I am not coding, I like to stay home, have a nice cup of coffee and hang out with my cats.",
+    location: "Toronto, ON",
+    skills: ["Java", "C++", "HTML5" ],
+    profession: "Software Engineer",
+    experience: "Mid Level - 2 years" ,
+    mentoredSessionsAmt: 20,
+    reviews: ["Best mentor ever!", "Raj is so cool", "Need a mentor? Talk to Raj."],
+    // timePreferences: ["9:00am", "2:30pm", "4:00pm"],
+  },
+
+];
+
+export default connectionsPageData;
