69f7e668db0043366e900e38fc3f2b9f5980e48f
Kati Ealey <kati.majorealey@gmail.com>
2024-02-18 21:51:47 -0500

    adds useNavigate to take user back to home page

--- src/components/ChatFeature/SidebarHeader/SidebarHeader.jsx
+++ src/components/ChatFeature/SidebarHeader/SidebarHeader.jsx
@@ -2,11 +2,20 @@ import React from "react"
 import "../SidebarHeader/Header.scss"
 import { FaArrowLeftLong, FaPlus } from "react-icons/fa6";
 import User from "../SidebarHeader/User.svg";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+
+    const navigate=useNavigate()
+
+    const handleBackArrow = () => {
+        navigate('/home')
+    }
+     
+
     return (
         <div className="headerContainer">
             <div className="header">
-                <div><FaArrowLeftLong size={30} className="backArrow" /></div>
+                <div><FaArrowLeftLong size={30} className="backArrow" onClick={handleBackArrow}/></div>
                 <h2 className="inboxText">Inbox</h2>
                 {/* <div className="userInfo"> */}
                     <img src={User} alt="user" className="userImg"/>
