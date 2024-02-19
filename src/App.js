f35a0b6d95fdc80f2aed3f2791e7325103277d9e
Yvv1214 <94neongreen@gmail.com>
2024-02-19 00:17:26 -0500

    remove unused dummy data

--- src/App.js
+++ src/App.js
@@ -53,24 +53,6 @@ const connectionsData = [
     profilePicture: "john.jpg",
     timePreferences: ["9:00am", "2:30pm", "4:00pm"],
   },
-  {
-    id: 2,
-    name: "Alice Smith",
-    profilePicture: "alice.jpg",
-    timePreferences: ["10:00am", "3:30pm", "5:00pm"],
-  },
-  {
-    id: 3,
-    name: "Julie Tall",
-    profilePicture: "julie.jpg",
-    timePreferences: ["9:30am", "3:00pm", "4:30pm"],
-  },
-  {
-    id: 5,
-    name: "George Small",
-    profilePicture: "george.jpg",
-    timePreferences: ["7:30am", "5:00pm", "7:30pm"],
-  },
 ];
 
 function App() {
