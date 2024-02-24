7528215a7a817a8dfa35905fdc64d41439a1d56a
Anthony Shellman <atshellman@gmail.com>
2024-02-24 11:45:50 -0700

    Add state, function and props

--- src/components/ChatFeature/Chat/Chat.jsx
+++ src/components/ChatFeature/Chat/Chat.jsx
@@ -1,6 +1,6 @@
 //where messages are sent back and forth
 
-import React from "react";
+import React, { useState } from "react";
 // import Messages from "./components/ChatFeature/Messages/Messages"
 import Messages from "../Messages/Messages"
 import Input from "../Input/Input"
@@ -14,27 +14,33 @@ import ChatData from "../../../data/chatData"
 const Chat =() => {
     const {users, messages} = ChatData
 
-    const navigate=useNavigate()
+    const [allMessages, setallMessages] = useState(messages);
+
+    const navigate = useNavigate()
 
     const handleBackArrowChat = () => {
         navigate('/inbox')
     }
 
-    return(
-    <div className="chat">
-        <div className="chatHeader">
-             <div className="colOne">
-            <FaArrowLeftLong size={30} className="backIcon" onClick={handleBackArrowChat} />
+    const addMessage = (newMessage) => {
+        setallMessages([...allMessages, newMessage])
+    }
+
+    return (
+        <div className="chat">
+            <div className="chatHeader">
+                <div className="colOne">
+                    <FaArrowLeftLong size={30} className="backIcon" onClick={handleBackArrowChat} />
+                </div>
+                <div className="colTwo">
+                    <img className="userTwoImg" src={users[1].avatar} alt="profile pic" />
+                    <span className="userName">{users[1].name}</span>
+                    <span className="userOccupation">{users[1].occupation}</span>
+                </div>
             </div>
-            <div className="colTwo">
-               <img className="userTwoImg" src={users[1].avatar} alt="profile pic" />
-                <span className="userName">{users[1].name}</span>
-                <span className="userOccupation">{users[1].occupation}</span> 
-            </div>
+            <Messages allMessages={allMessages} /> {/*passing the messages prop to messages component*/}
+            <Input allMessages={allMessages} addMessage={addMessage} avatar={users[0].avatar}/>
         </div>
-    <Messages messages={messages} /> {/*passing the messages prop to messages component*/}
-    <Input />
-    </div>
     )
 }
 
