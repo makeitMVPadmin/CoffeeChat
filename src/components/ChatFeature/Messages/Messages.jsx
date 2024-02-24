bcef1416c171723cc1f26c3cf44a4668e8bee9bd
Anthony Shellman <atshellman@gmail.com>
2024-02-24 11:48:12 -0700

    Remove unused import, update passed prop

--- src/components/ChatFeature/Messages/Messages.jsx
+++ src/components/ChatFeature/Messages/Messages.jsx
@@ -1,29 +1,26 @@
 //messages sent back and forth between users
 
 import React from "react";
-// import Message from "./components/ChatFeature/Message/Message"
 import Message from "../Message/Message"
 import"./Messages.scss"
 
 
 
-const Messages =({messages}) => {
+const Messages =({allMessages}) => {
     
-
-    const messagesList = messages.map((message) => {
-        return(
-            <Message 
-            key={message.id} 
-            content={message.content} 
-            sender={message.sender} 
-            userImg={message.avatar}
-            />
+    const messagesList = allMessages.map((message) => {
+        return (
+                <Message
+                key={message.id}
+                content={message.content}
+                sender={message.sender}
+                userImg={message.avatar}
+                />
             )
-    })
-        
-        
+        }
+    )
 
-    return(
+    return (
         <div className="messages">
             {messagesList}
         </div>
